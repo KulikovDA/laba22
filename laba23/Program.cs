97657811a9363d8eb8a6fb9cd5738d5bc09bc49e
Arion <Arion@DESKTOP-9BU80RS>
2023-10-15 23:43:59 +0200

    Добавьте файлы проекта.

---
+++ laba23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите вес");
+double m=double.Parse(Console.ReadLine());
+Console.WriteLine("Введите рост");
+double h=double.Parse(Console.ReadLine());
+if (h - 100 > m) Console.WriteLine("Надо похудеть");
+else if (h - 100 < m) Console.WriteLine("Надо поправится");
+else(h-100==m) Console.WriteLine("Вы идеальны");
